df945ac3f705319f1216ce736fa1d9d0ec49c811
JmmonJeremy <suc19002@byui.edu>
2026-06-17 20:38:16 -0700

    Added go services.

--- go-service/main.go
+++ go-service/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"time"
+)
+
+func statusHandler(w http.ResponseWriter, r *http.Request) {
+	response := map[string]string{
+		"service":   "Home Gig Go Service",
+		"status":    "Online",
+		"timestamp": time.Now().Format(time.RFC3339),
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
+}
 
 func main() {
-    fmt.Println("Hello from Go!")
+	http.HandleFunc("/status", statusHandler)
+
+	log.Println("Home Gig Go Service running on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
