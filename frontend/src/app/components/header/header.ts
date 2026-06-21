f636aab88508f6fd9c6d2f38acba642c162aabcd
JmmonJeremy <suc19002@byui.edu>
2026-06-20 19:21:42 -0700

    Fixed logout in the dropdown menu of the profile in the header.

--- frontend/src/app/components/header/header.ts
+++ frontend/src/app/components/header/header.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -7,5 +9,15 @@ import { Component } from '@angular/core';
   styleUrl: './header.css',
 })
 export class Header {
+constructor(
+  private authService: AuthService,
+  private router: Router
+) {}
 
+logout(event: MouseEvent): void {
+  event.preventDefault();
+
+  this.authService.logout();
+  this.router.navigate(['/login']);
+}
 }
