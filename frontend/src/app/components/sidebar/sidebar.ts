65652710bb9cb872106f0aa4e29a75312e60c398
JmmonJeremy <suc19002@byui.edu>
2026-06-20 19:04:43 -0700

    Fixed logout for the sibebar menu.

--- frontend/src/app/components/sidebar/sidebar.ts
+++ frontend/src/app/components/sidebar/sidebar.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -7,5 +9,15 @@ import { Component } from '@angular/core';
   styleUrl: './sidebar.css',
 })
 export class Sidebar {
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}
 
+logout(event: MouseEvent): void {
+  event.preventDefault();
+
+  this.authService.logout();
+  this.router.navigate(['/login']);
+}
 }
